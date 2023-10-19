5f2e272016e2a747cc51895020693705cd0ba367
mahlon.k.kirwa80 <usacys@local>
2023-10-19 13:30:51 -0400

    changes

--- Practise.py
+++ Practise.py
@@ -89,10 +89,56 @@ def disect(lst):
 
     finalList = tuple([tuple1,tuple2])
     print(finalList)
+
+def tough_read(fname):
+    """ Args: fname (str): path to a file where the input is located Returns: str: Sentence that was decoded """
+    integers = []
+    with open(fname, 'r') as f:
+        binary_string = f.readlines()
+
+
+    for b in binary_string:
+        integers.append(chr(int(b,base=2)))
+
+    # Convert the list of integers to a Unicode string
+    unicode_string = ''.join(integers)
     
+    return unicode_string
+
+def log_to_file(fname, theme):
+    '''
+    Args:
+        fname (str): Path to an existing file that includes previous inspirational messages to keep.
+        theme (str): Theme to be placed on each line.
+    Returns:
+        None
+    '''
+    # Open the file in 'append' mode
+    with open(fname, 'a') as file:
+        while True:
+            user_input = input("Enter an inspirational message (or leave it empty to finish): ")
+            if not user_input:
+                break  # Exit the loop if the input is empty
+            formatted_message = f'{theme}:{user_input}\n'  # Format the message
+            file.write(formatted_message)  # Write the message to the file
 
 
+
+def replace_in_file(in_path, out_path, reps):
+    # Read the input file and create a dictionary from the replacements
+    temporary_values = dict(reps)
+
+    # Open the input file for reading and the output file for writing
+    with open(in_path, 'r') as newOne, open(out_path, 'w') as newOneTwo:
+        # Iterate through each line in the input file
+        for line in newOne:
+            # Replace occurrences of words in the line using the dictionary
+            for find, replace in temporary_values.items():
+                line = line.replace(find, replace)
+            # Write the modified line to the output file
+            newOneTwo.write(line)
 
 if __name__== "__main__":
-    temp = [1,2,3,4,5,6]
-    disect(temp)
+    fname = 'assignment2.txt'
+    theme = 'Razzman'
+    print(tough_read(fname,theme))
